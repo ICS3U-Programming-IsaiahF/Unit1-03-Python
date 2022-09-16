80ff50fc82c02f7a12e303da28e69b75a9647173
Isaiah Fernandez <isaiah.fernandez@stu.ocsb.ca>
2022-09-16 08:31:24 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Isaiah Fernandez
+# Date: September 15, 2022
+# This program writes "Hello, World" in style
+
+
+def main():
+    print ("Hello, Ann")
+    print ("I love to code")
+
+
+if __name__ == "__main__":
+    main()
